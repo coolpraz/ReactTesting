b363ed05bd3bb0b253fac0679941e34de06224fc
Prabhat Shahi <praz.pos@gmail.com>
2025-03-25 23:01:36 +0545

    test button with correct text and color

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("App contains correct heading", () => {
+test("button starts with correct color", () => {
   render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
-  expect(headingElement).toBeInTheDocument();
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+  expect(buttonElement).toHaveClass("red");
+});
+
+test("button has correct color after click", () => {
+
+});
+
+test("button has correct text after click", () => {
+
 });
