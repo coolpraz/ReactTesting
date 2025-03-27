3a1f970d75cd594de5f325cea7251972eda9aba1
Prabhat Shahi <praz.pos@gmail.com>
2025-03-27 21:49:04 +0545

    button disabled on checkbox

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -19,3 +19,25 @@ test("button click flow", () => {
   // expect the button text to match /red/i
   expect(buttonElement).toHaveTextContent(/red/i);
 });
+
+test("checkbox flow", () => {
+  render(<App />);
+
+  // find elements
+  const buttonElement = screen.getByRole("button", { name: /blue/i });
+  const checkboxElement = screen.getByRole("checkbox", { name: /disable button/i });
+
+  // check initial conditions
+  expect(buttonElement).toBeEnabled();
+  expect(checkboxElement).not.toBeChecked();
+
+  // click checkbox to disable button
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeDisabled();
+  expect(checkboxElement).toBeChecked();
+
+  // click checkbox again to re-enable button
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeEnabled();
+  expect(checkboxElement).not.toBeChecked();
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [buttonColor, setButtonColor] = useState("red");
-  const nextColor = buttonColor === "red" ? "blue" : "red";
+    const [disabled, setDisabled] = useState(false);
+    const [buttonColor, setButtonColor] = useState("red");
+    const nextColor = buttonColor === "red" ? "blue" : "red";
 
-  return <div><button className={buttonColor} onClick={() => setButtonColor(nextColor)}>Change to {nextColor}</button></div>
+    return (
+        <div>
+            <button
+                className={buttonColor}
+                onClick={() => setButtonColor(nextColor)}
+                disabled={disabled}
+            >
+                Change to {nextColor}
+            </button>
+            <br />
+            <input
+                type="checkbox"
+                id="disable-button-checkbox"
+                defaultChecked={disabled}
+                onChange={(e) => setDisabled(e.target.checked)}
+            />
+            <label htmlFor="disable-button-checkbox">Disable button</label>
+        </div>
+    );
 }
 
-export default App
+export default App;
